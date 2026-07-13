55260687684576223441b675bfa1d257b55e844e
Angel Mandujano <anmandu@proton.me>
2026-07-12 22:49:55 -0600

    adding swagger openapi definition

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Dashboard API')
+    .setDescription('Endpoints del backend de mi dashboard personal')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
